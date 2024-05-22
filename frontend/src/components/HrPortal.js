6c6a7a3153f5d23c492e21145814ac94af39dbfa
vikramvirwani <vikramvirwani2000@gmail.com>
2024-05-22 20:05:42 +0530

    frontend

--- frontend/src/components/HrPortal.js
+++ frontend/src/components/HrPortal.js
@@ -17,14 +17,16 @@ function HrPortal() {
 
   return (
     <div>
-        <div className='mt-10 flex flex-col gap-5 items-center justify-center text-white'>
+        <div className='mt-10 pb-5 flex flex-col gap-5 items-center justify-center text-white'>
             <h1 className='text-5xl font-bold'>
                 See who has applied to your job opening
             </h1>
         </div>
-        {HrDummyData.map((data)=>(
-            <HrCard key={data.applicant_id} {...data}/>
-        ))}
+        <div className='pt-5'>
+            {HrDummyData.map((data)=>(
+                <HrCard key={data.applicant_id} {...data}/>
+            ))}
+        </div>
         {/* <HrCard/> */}
     </div>
   )
--- frontend/src/components/component/HrCard/index.jsx
+++ frontend/src/components/component/HrCard/index.jsx
@@ -15,13 +15,21 @@ function HrCard(props) {
 
   return (
     <div>
-        <div className='mx-40 mb-4 pt-10'>
-            <div className='flex justify-between items-center px-6 py-4 bg-zinc-200 rounded-md border border-black shdow-lg hover:border-blue-500 hover:translate-y-1 hover:scale-105'>
-                <div>
-                    <h1>Job id: {props.applicant_id}</h1>
-                    <h1>Name: {props.Name}</h1>
-                    <h1>Age: {props.Age}</h1>
-                    <h1>Experience: {props.Experience} Year</h1>
+        <div className='mx-40 mb-4'>
+            <div className='flex justify-between items-center px-6 py-4 bg-zinc-200 rounded-md border border-black shadow-lg hover:border-blue-500 hover:translate-y-1 hover:scale-105'>
+                <div className='flex space-x-20'>
+                    <h1 className='text-lg font-semibold text-gray-800'>
+                        <span className='font-bold text-[#3b0764]'>Job ID:</span> {props.applicant_id}
+                    </h1>
+                    <h1 className='text-lg font-semibold text-gray-800'>
+                        <span className='font-bold text-[#3b0764]'>Name:</span> {props.Name}
+                    </h1>
+                    <h1 className='text-lg font-semibold text-gray-800'>
+                        <span className='font-bold text-[#3b0764]'>Age:</span> {props.Age}
+                    </h1>
+                    <h1 className='text-lg font-semibold text-gray-800'>
+                        <span className='font-bold text-[#3b0764]'>Experience:</span> {props.Experience} Year{props.Experience > 1 ? 's' : ''}
+                    </h1>
                 </div>
                 <div>
                     <button className='text-blue-500 hover:bg-blue-300 border border-blue-500 px-10 py-2 rounded-md' onClick={handleShow}>Show More</button>
@@ -30,42 +38,43 @@ function HrCard(props) {
         </div>
         <Dialog open={open} fullWidth>
             <DialogContent>
-                <Grid container spacing={2}>
-                    <Grid item xs={6} className="bg-gray-200 p-4 m-6 shadow-md flex items-center justify-center">
-                        <Typography variant="h6" component="h1">
-                            Name: {props.Name}
-                        </Typography>
+                <div className="p-4 m-6 rounded-md">
+                    <Grid container spacing={2}>
+                        <Grid item xs={6} className="bg-gray-200 border-2 border-[#3b0764] p-4 shadow-md flex items-center justify-center">
+                            <Typography variant="h6" component="h1">
+                                <span className='font-bold text-[#3b0764]'>Name:</span> {props.Name}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} className="bg-gray-200 border-2 border-[#3b0764] p-4 shadow-md flex items-center justify-center">
+                            <Typography variant="h6" component="h1">
+                                <span className='font-bold text-[#3b0764]'>Age:</span> {props.Age}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} className="bg-gray-200 border-2 border-[#3b0764] p-4 shadow-md flex items-center justify-center">
+                            <Typography variant="h6" component="h1">
+                                <span className='font-bold text-[#3b0764]'>Experience:</span> {props.Experience} year
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} className="bg-gray-200 border-2 border-[#3b0764] p-4 shadow-md flex flex-col items-center justify-center">
+                            <Typography variant="h6" component="h1">
+                                <span className='font-bold text-[#3b0764]'>Resume:</span>
+                            </Typography>
+                            <a href={props.resume} download className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
+                                Download
+                            </a>
+                        </Grid>
+                        <Grid item xs={12} className="bg-gray-200 border-2 border-[#3b0764] p-4 shadow-md flex items-center justify-center">
+                            <Typography variant="h6" component="h1">
+                                <span className='font-bold text-[#3b0764]'>Address:</span> {props.address}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} className="bg-gray-200 border-2 border-[#3b0764] p-4 shadow-md flex items-center justify-center">
+                            <Typography variant="h6" component="h1">
+                                <span className='font-bold text-[#3b0764]'>Description:</span> {props.Description}
+                            </Typography>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6} className="bg-gray-200 p-4 m-2 shadow-md flex items-center justify-center">
-                        <Typography variant="h6" component="h1">
-                            Age: {props.Age}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={6} className="bg-gray-200 p-4 m-2 shadow-md flex items-center justify-center">
-                        <Typography variant="h6" component="h1">
-                            Experience: {props.Experience} year
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={6} className="bg-gray-200 p-4 m-2 shadow-md flex flex-col items-center justify-center">
-                        <Typography variant="h6" component="h1">
-                            Resume : 
-                        </Typography>
-                        <a href={props.resume} download className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
-                            Download
-                        </a>
-                    </Grid>
-
-                    <Grid item xs={12} className="bg-gray-200 p-4 m-2 shadow-md flex items-center justify-center">
-                        <Typography variant="h6" component="h1">
-                            Address: {props.address}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} className="bg-gray-200 p-4 m-2 shadow-md flex items-center justify-center">
-                        <Typography variant="h6" component="h1">
-                            Description: {props.Description}
-                        </Typography>
-                    </Grid>
-                </Grid>
+                </div>
             </DialogContent>
             <DialogActions>
                 <Box className="text-blue-500 hover:bg-blue-300 border border-blue-500 px-10 py-2 rounded-md">
